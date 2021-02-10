41fe7b503b591de5c568d0e64905ed775c1bdc56
goldenglorys <olusolagloryolamide@gmail.com>
2021-02-10 13:15:58 +0100

    ES6 object destructuring feature implememted to the HackerNews components innner state data

--- src/HackerNews.js
+++ src/HackerNews.js
@@ -43,33 +43,32 @@ class HackerNews extends Component {
   }
 
   render() {
+    const { searchTerm, list } = this.state;
     return (
       <div>
         <form>
           <input type="text" onChange={this.onSearchChange} />
         </form>
-        {this.state.list
-          .filter(isSearched(this.state.searchTerm))
-          .map((item) => {
-            return (
-              <div key={item.objectID}>
-                <span>
-                  <a href={item.url}>{item.title}</a>
-                </span>
-                <span>{item.author}</span>
-                <span>{item.num_comments}</span>
-                <span>{item.points}</span>
-                <span>
-                  <button
-                    onClick={() => this.onDismiss(item.objectID)}
-                    type="button"
-                  >
-                    Dismiss
-                  </button>
-                </span>
-              </div>
-            );
-          })}
+        {list.filter(isSearched(searchTerm)).map((item) => {
+          return (
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+              <span>
+                <button
+                  onClick={() => this.onDismiss(item.objectID)}
+                  type="button"
+                >
+                  Dismiss
+                </button>
+              </span>
+            </div>
+          );
+        })}
       </div>
     );
   }
