29191376feabc5f51cf38cefcea066988b5c416b
goldenglorys <olusolagloryolamide@gmail.com>
2021-02-23 15:21:11 +0100

    Passing dummy todo data from the app comp to the sub todo comp through props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import HackerNewsAPI from "./components/HackerNews/HackerNewsAPI";
+import React, { Component } from "react";
+// import HackerNewsAPI from "./components/HackerNews/HackerNewsAPI";
+import Todos from "./components/TodoApp/Todos";
 
-function App() {
-  return (
-    <div>
-      <HackerNewsAPI></HackerNewsAPI>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Todo one",
+        completed: false,
+      },
+      {
+        id: 2,
+        title: "Todo two",
+        completed: false,
+      },
+      {
+        id: 3,
+        title: "Todo three",
+        completed: false,
+      },
+    ],
+  };
+  render() {
+    return (
+      <div>
+        {/* <HackerNewsAPI></HackerNewsAPI> */}
+        <Todos todos={this.state.todos} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/TodoApp/Todo.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class Todo extends Component {
-  render() {
-    return <div></div>;
-  }
-}
-
-export default Todo;
