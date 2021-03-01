8c4f228c65f40f85ea75b68ad3691a9c661df229
goldenglorys <olusolagloryolamide@gmail.com>
2021-03-01 16:24:10 +0100

    Realtime DB: Adding reg user to db after auth add and getting list of added users to the admin panel

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,5 +1,6 @@
 import app from "firebase/app";
 import "firebase/auth";
+import "firebase/firebase-database";
 
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -13,9 +14,10 @@ class Firebase {
   constructor() {
     app.initializeApp(config);
     this.auth = app.auth();
+    this.db = app.database();
   }
 
-  // * AUTH API *
+  // *** AUTH API ***
 
   createUserWithEmailAndPassword = (email, password) =>
     this.auth.createUserWithEmailAndPassword(email, password);
@@ -28,6 +30,12 @@ class Firebase {
   passwordReset = (email) => this.auth.sendPasswordResetEmail(email);
 
   passwordUpdate = (password) => this.auth.currentUser.updatePassword(password);
+
+  // *** USER API ***
+
+  user = (uid) => this.db.ref(`users/${uid}`);
+
+  users = () => this.db.ref("users");
 }
 
 export default Firebase;
