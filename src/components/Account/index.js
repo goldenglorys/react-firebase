1c2aa7a68773c953867f735afec60fe9861757d5
goldenglorys <olusolagloryolamide@gmail.com>
2021-03-01 15:35:53 +0100

    Authorization: General authorization and path protection feature added

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,7 +1,20 @@
 import React from "react";
 
-function Account() {
-  return <div></div>;
-}
+import { AuthUserContext, withAuthorization } from "../Session";
+import { PasswordForgetForm } from "../PasswordForget";
+import PasswordChangeForm from "../PasswordChange";
 
-export default Account;
+const AccountPage = () => (
+  <AuthUserContext.Consumer>
+    {(authUser) => (
+      <div>
+        <h1>Account: {authUser.email}</h1>
+        <PasswordForgetForm />
+        <PasswordChangeForm />
+      </div>
+    )}
+  </AuthUserContext.Consumer>
+);
+
+const condition = (authUser) => !!authUser;
+export default withAuthorization(condition)(AccountPage);
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,10 @@
 import React from "react";
-
-function Home() {
-  return <div></div>;
-}
-
-export default Home;
+import { withAuthorization } from "../Session";
+const HomePage = () => (
+  <div>
+    <h1>Home Page</h1>
+    <p>The Home Page is accessible by every signed in user.</p>
+  </div>
+);
+const condition = (authUser) => !!authUser;
+export default withAuthorization(condition)(HomePage);
